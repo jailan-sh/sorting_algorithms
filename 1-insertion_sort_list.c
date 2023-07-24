d779764732016344d310527f4ee2b79b8edeb8bf
ghaidaareda <dodo.sa7afy@gmail.com>
2023-07-24 07:10:32 -0700

    insertion_sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ *insertion_sort_list-sorts doubly linked list in ascending order with Insertion sort
+ *@list:pointer to pointer to a list
+ *Return:void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp = NULL;
+        listint_t *current = NULL;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	current = (*list)->next;
+	while (current)
+	{
+		temp = current->prev;
+		while (temp != NULL && temp->n > current->n)
+		{
+			temp->next = current->next;
+			if (current->next != NULL)
+			{
+				current->next->prev = temp;
+			}
+			current->next = temp;
+			current->prev = temp->prev;
+			temp->prev = current;
+			if (current->prev == NULL)
+			{
+				*list = current;
+			}
+			else
+			{
+				current->prev->next = current;
+			}
+			print_list(*list);
+			temp = current->prev;
+		}
+		current = current->next;
+	}
+}
+
+
